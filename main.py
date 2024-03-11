39c384904e3d53095c07bde3a8dc251419258e03
Negrub Andrii <zurg0505@gmail.com>
2024-03-11 20:34:59 +0200

    Done tasks from point #4.8

--- main.py
+++ main.py
@@ -7,20 +7,20 @@ def main():
     output_to_console("You entered: " + console_text)
 
     try:
-        builtin_text = read_from_file_builtin('Titanic-Dataset.csv')
+        builtin_text = read_from_file_builtin('data/Titanic-Dataset.csv')
         output_to_console("\nContent of Titanic-Dataset.csv read using built-in functions:")
         output_to_console(builtin_text[:300] + "\n")
-        write_to_file_builtin(builtin_text, 'Builtin_Titanic-Dataset.csv')
-        output_to_console("Content written to Builtin_Titanic-Dataset.csv using built-in functions.")
+        write_to_file_builtin(builtin_text, 'data/Builtin_Titanic-Dataset.csv')
+        output_to_console("Content written to data/Builtin_Titanic-Dataset.csv using built-in functions.")
     except Exception as e:
         output_to_console("An error occurred with built-in file functions: " + str(e))
 
     try:
-        pandas_data = read_from_file_pandas('Titanic-Dataset.csv')
+        pandas_data = read_from_file_pandas('data/Titanic-Dataset.csv')
         output_to_console("Content of Titanic-Dataset.csv read using pandas:")
         output_to_console(pandas_data.head().to_string())
-        write_to_file_pandas(pandas_data.head(), 'Pandas_Titanic-Dataset.csv')
-        output_to_console("Content written to Pandas_Titanic-Dataset.csv using pandas.")
+        write_to_file_pandas(pandas_data.head(), 'data/Pandas_Titanic-Dataset.csv')
+        output_to_console("Content written to data/Pandas_Titanic-Dataset.csv using pandas. ")
     except Exception as e:
         output_to_console("An error occurred with pandas file functions: " + str(e))
 
