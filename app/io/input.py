27a53e4bc2ed3e8a333ac647a1d10a6946378f46
Negrub Andrii <zurg0505@gmail.com>
2024-03-11 21:11:27 +0200

    Done tasks from point #4.8

--- app/io/input.py
+++ app/io/input.py
@@ -2,16 +2,45 @@ import pandas as pd
 
 
 def input_from_console():
+    """
+    Prompts the user to input text from the console.
 
+    Returns:
+        str: The text entered by the user.
+    """
     return input("Please enter text: ")
 
 
 def read_from_file_builtin(filename):
+    """
+    Reads content from a file using Python's built-in functionality.
 
+    Args:
+        filename (str): The path to the file to be read.
+
+    Returns:
+        str: The content of the file as a string.
+
+    Raises:
+        FileNotFoundError: If the file does not exist.
+        IOError: If the file could not be opened.
+    """
     with open(filename, 'r') as file:
         return file.read()
 
 
 def read_from_file_pandas(filename):
+    """
+    Reads content from a CSV file using the pandas library.
 
+    Args:
+        filename (str): The path to the CSV file to be read.
+
+    Returns:
+        DataFrame: A pandas DataFrame containing the data from the CSV file.
+
+    Raises:
+        FileNotFoundError: If the CSV file does not exist.
+        ValueError: If the CSV file is empty or has invalid content.
+    """
     return pd.read_csv(filename)
--- app/io/output.py
+++ app/io/output.py
@@ -2,16 +2,39 @@ import pandas as pd
 
 
 def output_to_console(text):
+    """
+    Outputs the given text to the console.
 
+    Args:
+        text (str): The text to be printed to the console.
+    """
     print(text)
 
 
 def write_to_file_builtin(text, filename):
+    """
+    Writes the given text to a file using Python's built-in functionality.
 
+    Args:
+        text (str): The text to be written to the file.
+        filename (str): The path to the file where the text will be written.
+
+    Raises:
+        IOError: If the file could not be opened for writing.
+    """
     with open(filename, 'w') as file:
         file.write(text)
 
 
 def write_to_file_pandas(data, filename):
+    """
+    Writes the given DataFrame to a CSV file using the pandas library.
 
+    Args:
+        data (DataFrame): The pandas DataFrame to be written to the CSV file.
+        filename (str): The path to the CSV file where the DataFrame will be written.
+
+    Raises:
+        IOError: If the CSV file could not be opened for writing.
+    """
     data.to_csv(filename, index=False)
